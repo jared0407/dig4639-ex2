26e4e2348491d973410b14a4408bc08e68080342
Jared0407 <jaredbrennan88@knights.ucf.edu>
2021-02-06 20:42:20 -0500

    closes #1

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let quotes = [
     '“Don’t let the noise of others’ opinions drown out your own inner voice. ― Steve Job"',
 
      '"All our dreams can come true, if we have the courage to pursue them.- Walt Disney"',
+
+     '"Work isnt WORK unless you would rather be DOING something else,"- Don Shula"',
 ]
-let index = Math.floor(Math.random() * 2)
+let index = Math.floor(Math.random() * 3)
 console.log(quotes[index])
