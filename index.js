0d8eb4fe17f375961328b46da4e1d339bce7a004
Jared0407 <jaredbrennan88@knights.ucf.edu>
2021-02-06 20:37:38 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    '“Don’t let the noise of others’ opinions drown out your own inner voice. ― Steve Job"',
+
+     '"All our dreams can come true, if we have the courage to pursue them.- Walt Disney"',
+]
+let index = Math.floor(Math.random() * 2)
+console.log(quotes[index])
